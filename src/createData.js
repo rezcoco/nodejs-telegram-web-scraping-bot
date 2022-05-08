c32fee2eff1c7209ac87a2674a8440273d5210a0
Rez <77873738+rezcoco@users.noreply.github.com>
2022-05-08 10:08:39 +0700

    fix

--- src/createData.js
+++ src/createData.js
@@ -5,37 +5,35 @@ const writeData = data => {
   const { name, link } = data
 
   if (!fs.existsSync(dirPath)) {
-    fs.mkdirSync(dirPath);
-    fs.writeFile(dirPath, '[]', err => {
-      if (err) console.log(err)
-    })
-  } else {
-     fs.readFile(dirPath, 'utf-8', (err, data) => {
-     if (err) console.log(err)
-  
-     const scrapedData = { name, link }
-     const readed = JSON.parse(data)
-  
-     // check duplicate data
-     for (let i = 0; i < readed.length; i++) {
-       if (name === readed[i].name) {
-         console.log('data already written')
-         get(readed)
-         return
-       }
-     }
-     // added to json
-     readed.push(scrapedData)
-  
-     // write data
-     fs.writeFile(dirPath, JSON.stringify(readed), err => {
-       if (err) console.log(err)
-  
-       console.log("Written")
-       get(readed)
-     })
-   })
+    try {
+      fs.mkdirSync(dirPath);
+      fs.writeFileSync(dirPath, '[]','utf-8')
+    } catch (err) {
+      console.log(err)
+    }
   }
+  fs.readFile(dirPath, 'utf-8', (err, data) => {
+    if (err) console.log(err)
+    
+    const scrapedData = { name, link }
+    const readed = JSON.parse(data)
+    
+    // check duplicate data
+    for (let i = 0; i < readed.length; i++) {
+     if (name === readed[i].name) {
+       console.log('data already written')
+       return
+     }
+    }
+    // added to json
+    readed.push(scrapedData)
+    
+    // write data
+    fs.writeFile(dirPath, JSON.stringify(readed), err => {
+     if (err) console.log(err)
+     console.log("Written")
+    })
+  })
 }
 
 module.exports.writeData = writeData
