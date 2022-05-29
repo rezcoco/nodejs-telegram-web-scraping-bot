4ad6f34dcbd96e1fdc18baf59d12c833c26238e5
Rez <77873738+rezcoco@users.noreply.github.com>
2022-05-29 14:21:15 +0700

    Update alive.js

--- src/alive.js
+++ src/alive.js
@@ -3,12 +3,10 @@ const { getLink: ping } = require('./api')
 
 console.log(BASE_URL)
 
-if (BASE_URL) {
-  const alive = () => {
-    const time = new Date()
-    const hours = time.getHours(), minutes = time.getMinutes()
-    ping(BASE_URL)
-    console.log(`Waked up at: ${hours}:${minutes}`)
-  }
-  setInterval(alive, 1 * 60 * 1000)
-} 
+const alive = () => {
+  const time = new Date()
+  const hours = time.getHours(), minutes = time.getMinutes()
+  ping(BASE_URL)
+  console.log(`Waked up at: ${hours}:${minutes}`)
+}
+setInterval(alive, 1 * 60 * 1000)
